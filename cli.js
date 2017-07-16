d6e8b5fff9e08e777819bb28b0c974f4ba466e06
mario.nebl@sinnerschrader.com <mario.nebl@sinnerschrader.com>
2017-07-16 20:53:15 +0200

    implement css-animated terminal sequences

--- cli.js
+++ cli.js
@@ -1,7 +1,72 @@
 #!/usr/bin/env node
-'use strict'
+const meow = require('meow');
+const getStdin = require('get-stdin');
+const toString = require('vdom-to-html')
 
-const minimist = require('minimist')
+const pkg = require('./package');
+const renderAt = require('.');
+const {animate} = require('.');
+
+withCli(main, `
+	Usage:
+		asciicast-to-svg
+
+	Options:
+		--animate
+
+	Examples:
+		cat asciicast.json | asciicast-to-svg
+		asciicast-to-svg < asciicast.json
+`);
+
+function main(cli) {
+	const error = cliError(cli);
+	const input = parseFloat(cli.input[0]);
+
+	return getStdin()
+		.then(stdin => {
+			if (!stdin && !cli.flags.file) {
+				throw error('stdin [input] is required');
+			}
+
+			const cast = JSON.parse(stdin);
+
+			if (cli.flags.animate) {
+				return emit(animate(cast));
+			}
+
+			const render = (at, options) => renderAt(cast, at, options);
+			const at = Number.isNaN(input) ? cast.duration : input;
+			return emit(render(at));
+		});
+}
+
+function cliError(cli) {
+	return message => {
+		const err = new Error(message);
+		err.help = cli.showHelp;
+		err.controlled = true;
+		return err;
+	};
+}
+
+function emit(svg) {
+	return toString(svg).replace('<svg ', '<svg xmlns="http://www.w3.org/2000/svg" ');
+}
+
+function withCli(main, help) {
+	return main(meow(help))
+		.then(output => output && console.log(output))
+		.catch(err => {
+			if (err.controlled && err.help) {
+				console.error(err.message);
+				return err.help(1);
+			}
+			throw err;
+		});
+}
+
+/* const minimist = require('minimist')
 const toString = require('vdom-to-html')
 
 const pkg = require('./package.json')
@@ -51,4 +116,4 @@ process.stdin
 	} catch (err) {
 		return showError(err)
 	}
-})
+}) */
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 'use strict'
 
 const Terminal = require('headless-terminal')
-const colors = require('./colors')
-const h = require('virtual-dom/virtual-hyperscript/svg')
+const colors = require('./colors');
+const h = require('virtual-dom/virtual-hyperscript/svg');
+const diff = require('virtual-dom/diff');
+const hash = require('shorthash');
+const {omit, isEqual} = require('lodash');
+
+module.exports = renderAt
+module.exports.renderAt = renderAt;
+module.exports.animate = animate;
+module.exports.createRenderer = createRenderer;
 
 // bit masks, see https://github.com/dtinth/screen-buffer#cell-attributes
 const flags = [
@@ -23,84 +31,221 @@ for (let [name, size] of flags) {
 	offset += size
 }
 
-const projectX = (x) => Math.round(x * .5 * 2000) / 100
-const projectY = (y) => Math.round(y * 2000) / 100
-
-const renderCell = (x, y, text, fg, bg, inverse, underline, bold) => {
-	const s = {fill: fg}
-	if (underline) s.textDecoration = 'underline'
-	if (bold) s.fontWeight = 'bold'
-	if (bg && bg !== '#000') s.backgroundColor = bg
-
-	// todo: inverse
-	return h('text', {
-		x: projectX(x) + '',
-		y: projectY(y + .8) + '',
-		style: s
-	}, text)
-}
-
-const createRenderer = (meta) => {
-	const t = new Terminal(meta.width, meta.height)
-
-	const write = (diff) => {
-		t.write(diff)
-	}
-
-	const text = () => t.displayBuffer.toString()
-
-	const render = () => {
-		const b = t.displayBuffer
-
-		const cells = []
-		for (let y = 0; y < meta.height; y++) {
-			for (let x = 0; x < meta.width; x++) {
-				const [raw, text] = b.getCell(y, x)
-				if (!text || text === ' ') continue
-
-				const inverse = !!((raw & masks.inverse) >> shifts.inverse)
-				const underline = !!((raw & masks.underline) >> shifts.underline)
-				const bold = !!((raw & masks.bold) >> shifts.bold)
-				const fg = colors[(raw & masks.fg) >> shifts.fg] || '#fff'
-				const bg = colors[(raw & masks.bg) >> shifts.bg]
-
-				cells.push(renderCell(x, y, text, fg, bg, inverse, underline, bold))
-			}
-		}
-
-		const width = projectY(.5) + projectX(meta.width)
-		const height = projectY(.5) + projectY(meta.height)
-
-		return h('svg', {
-			xmlns: 'http://www.w3.org/2000/svg',
-			width: width + '',
-			height: height + '',
-			viewBox: [-projectY(.25), -projectY(.25), width, height].join(' '),
-			style: {
-				backgroundColor: '#000',
-				fontFamily: "Consolas, Menlo, 'Bitstream Vera Sans Mono', monospace, 'Powerline Symbols'",
-				fontSize: projectY(.8),
-				stroke: 'none'
-			}
-		}, cells)
-	}
-
-	return {write, text, render}
-}
-
-const renderAt = (asciicast, time) => {
+function renderAt(asciicast, time) {
 	const renderer = createRenderer(asciicast)
 
 	let t = 0
 	for (let [delay, data] of asciicast.stdout) {
 		t += delay
 		if (t > time) break
-
 		renderer.write(data)
 	}
 
-	return renderer.render()
+	return renderer.render();
 }
 
-renderAt.createRenderer = createRenderer
-module.exports = renderAt
+function animate(cast) {
+	const renderer = createRenderer(cast);
+	const delays = [];
+
+	const frames = cast.stdout
+		.map(([delay, data], i) => {
+			renderer.write(data);
+			delays.push(delay);
+			const start = delays.reduce((a, d) => a += d, 0);
+			const next = cast.stdout[i + 1];
+			const end = next ? start + next[0] : cast.duration;
+			return renderer.render({component: 'g', start, end});
+		});
+
+	return Screen({width: cast.width, height: cast.height}, frames);
+}
+
+
+function matrix(b) {
+	let rows = [];
+
+	for (let y = 0; y < b.getRows(); y++) {
+		rows[y] = [];
+
+		for (let x = 0; x < b.getCols(y); x++) {
+			const [raw, children] = b.getCell(y, x);
+			if (!children) continue;
+
+			const inverse = !!((raw & masks.inverse) >> shifts.inverse);
+			const underline = !!((raw & masks.underline) >> shifts.underline);
+			const bold = !!((raw & masks.bold) >> shifts.bold);
+			const fg = colors[(raw & masks.fg) >> shifts.fg] || '#fff';
+			const bg = colors[(raw & masks.bg) >> shifts.bg] || '#000';
+
+			if (children === ' ') {
+				const [, pre] = b.getCell(y, x - 1);
+				const [, post] = b.getCell(y, x + 1);
+				if (!pre || pre === ' ' || !post || post === ' ') {
+					continue;
+				}
+			}
+
+			const props = {x, y, fg, bg, inverse, underline, bold};
+			rows[y].push({props, children});
+		}
+	}
+
+	return rows;
+}
+
+function createRenderer(meta) {
+	const t = new Terminal(meta.width, meta.height)
+
+	const write = (diff) => t.write(diff);
+	const text = () => b.toString();
+
+	const render = (props = {}) => {
+		const rows = group(matrix(t.displayBuffer));
+		const cells = rows.reduce((rows, groups) => [...rows, ...groups.map(g => Group(g.props, g.children))], []);
+
+		if (!props.component || props.component === 'svg') {
+			return Screen({width: meta.width, height: meta.height}, cells);
+		} else {
+			if (cells.length === 0) {
+				return null;
+			}
+
+			const startFrame = props.start / (meta.duration / 100);
+			const endFrame = props.end / (meta.duration / 100);
+
+			const name = `frame-${String(props.start).replace('.', '-')}`;
+			return h('g', {class: `${name} frame`}, [
+				h('style', {type: 'text/css'}, [
+				`
+				.${name} {
+					opacity: 0;
+					animation-name: ${name};
+					animation-duration: ${meta.duration}s;
+					animation-timing-function: steps(1, end);
+					animation-iteration-count: infinite;
+				}
+				@keyframes ${name} {
+					from {
+						opacity: 0;
+					}
+					${startFrame}% {
+						opacity: 1;
+					}
+					${endFrame}% {
+						opacity: 1;
+					}
+					to {
+						opacity: 0;
+					}
+				}
+				`
+				]),
+				h('rect', {
+					width: scale(meta.width),
+					height: scale(meta.height),
+					style: {
+						fill: '#000'
+					}
+				}),
+				...cells
+			]);
+		}
+	}
+
+	return {write, text, render};
+}
+
+const emitted = [];
+
+function Group(props, children) {
+	const x = projectX(props.x);
+	const y = projectY(props.y + .8);
+
+	const style = {
+		fill: props.fg ? props.fg : null,
+		fontWeight: props.bold ? 'bold' : null,
+		textDecoration: props.underline ? 'underline' : null
+	};
+
+	const css = [
+		style.fill && `fill: ${style.fill}`,
+		style.fontWeight && `font-weight: ${style.fontWeight}`,
+		style.textDecoration && `text-decoration: ${style.textDecoration}`
+	].filter(Boolean).join(';');
+
+	const has = css.length > 0;
+	const id = has ? `t${hash.unique(JSON.stringify(style))}` : null;
+
+	const emit = emitted.includes(id) || !has
+		? null
+		: h('style', {}, `.${id} {${css}}`);
+
+	if (emit) {
+		emitted.push(id);
+	}
+
+	return h('text', {
+		className: has ? id : null,
+		x: String(x),
+		y: String(y)
+	}, [
+		emit,
+		children
+	])
+}
+
+function Screen(props = {}, children = []) {
+	const width = scale(props.width);
+	const height = scale(props.height);
+
+	return h('svg', {
+		xmlns: 'http://www.w3.org/2000/svg',
+		width: String(width),
+		height: String(height),
+		viewBox: [-projectY(.25), -projectY(.25), width, height].join(' '),
+		style: {
+			backgroundColor: '#000',
+			fontFamily: "Consolas, Menlo, 'Bitstream Vera Sans Mono', monospace, 'Powerline Symbols'",
+			fontSize: projectY(.8),
+			stroke: 'none'
+		}
+	}, children);
+}
+
+function group(rows) {
+	return rows.map(cells => cells.reduce((groups, cell, i) => {
+		const prev = groups[groups.length - 1] || {children: ''};
+
+		if (prev.children.length === 0) {
+			prev.props = cell.props;
+			prev.children = cell.children;
+			groups.push(prev);
+			return groups;
+		}
+
+		if (isEqual(omit(cell.props, ['x', 'y']), omit(prev.props, ['x', 'y']))) {
+			prev.children += cell.children;
+			return groups;
+		}
+
+		groups.push({
+			props: cell.props,
+			children: cell.children
+		});
+
+		return groups;
+	}, []));
+}
+
+function scale(dim) {
+	return projectY(.5) + projectX(dim);
+}
+
+function projectX (x) {
+	return Math.round(x * .5 * 2000) / 100;
+}
+
+function projectY (y) {
+	return Math.round(y * 2000) / 100;
+}
